e177a2473bdc7421a7e37e742f2c6945ffdd9db9
Aayush Atharva <aayush@shieldblaze.com>
2026-07-10 00:01:45 +0530

    Add Spring Project

---
+++ src/test/java/io/sessionlayer/controlplane/ControlplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sessionlayer.controlplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
